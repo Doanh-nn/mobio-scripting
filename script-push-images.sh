eed227313d3607f8cc486fe7a9a46678cef26aab
LoiND <danhloi2210@gmail.com>
2022-07-08 13:35:03 +0700

    split script: pull image + push image

--- script-push-images.sh
+++ script-push-images.sh
@@ -6,14 +6,15 @@ REGISTRY_MOBIO_URI="registry.mobio.vn"
 REGISTRY_MOBIO_USER=vib
 REGISTRY_MOBIO_PASS="Vib@$^*1357"
 
-docker login -u="${REGISTRY_MOBIO_USER}" -p="${REGISTRY_MOBIO_PASS}" ${REGISTRY_MOBIO_URI}
-
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'list_module_info=($(cat $(pwd)/module.txt))'
 
 function retag_image(){
     new_image=$(sed "s|${REGISTRY_MOBIO_URI}|${VIB_REGISTRY_URI}|g" <<< "$image")
 
-    docker pull $image
+    docker tag $image $new_image
+    docker push $new_image
+    docker rmi $image
+    docker rmi $new_image
 }
 
 for fields in ${list_module_info[@]}
@@ -27,5 +28,3 @@ do
         retag_image $folder $image
     fi
 done
-
-docker logout ${REGISTRY_MOBIO_URI}
