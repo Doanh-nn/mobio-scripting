3b770e849bd66814b6be5a2a590724be05770469
LoiND <danhloi2210@gmail.com>
2022-08-11 14:14:54 +0700

    update script: add login azure acr

--- scripting/script-push-images.sh
+++ scripting/script-push-images.sh
@@ -6,13 +6,21 @@ REGISTRY_MOBIO_URI="registry.mobio.vn"
 REGISTRY_MOBIO_USER=vib
 REGISTRY_MOBIO_PASS="Vib@$^*1357"
 
+docker login -u="${REGISTRY_MOBIO_USER}" -p="${REGISTRY_MOBIO_PASS}" ${REGISTRY_MOBIO_URI}
+
+az login --service-principal -u $(aksAppID) -p $(aksAppSecret) --tenant $(tenantID)
+az acr login -n $(acrName)
+
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'list_module_info=($(cat $(pwd)/module.txt))'
 
 function push_image(){
     new_image=$(sed "s|${REGISTRY_MOBIO_URI}|${VIB_REGISTRY_URI}|g" <<< "$image")
 
+    docker pull $image
     docker tag $image $new_image
+
     docker push $new_image
+
     docker rmi $image
     docker rmi $new_image
 }
@@ -30,3 +38,5 @@ do
         push_image $folder $image
     fi
 done
+
+docker logout ${REGISTRY_MOBIO_URI}
